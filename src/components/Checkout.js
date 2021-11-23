9f6d6e5f944a22947b81521bc0caab7209c636fb
212690308 <sunilkumar.yadav@ge.com>
2021-11-23 13:17:25 +0530

    controlled and uncontrolled

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,27 +1,37 @@
 import React from 'react';
 
 class Checkout extends React.Component {
-
-
-    saveData(e) {
-        e.preventDefault();
-        console.log("Form submission Logic");
-    }
-
-    render() {
-
-        return(
-            <div>
-                <form onSubmit={(ev)=> this.saveData(ev)}>
-                    <div>
-                        <label>Name</label>
-                        <input type="text" name="name"></input>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-
-            </div>
-        );
-    }
+  state = { name: '' };
+  emailRef = null;
+  saveData(e) {
+    e.preventDefault(); // cancel the default behavior of the event
+    console.log('FORM SUBMISSION LOGIC HERE', this.state, this.emailRef.value);
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={(ev) => this.saveData(ev)}>
+          <div>
+            <label>Name</label>
+            {/* CONTROLLED */}
+            <input
+              type="text"
+              onBlur={(e) => this.setState({ name: e.target.value })}
+            />
+            {this.state.name === '' ? <small>Name is required.</small> : null}
+          </div>
+          <div>
+            <label>Email</label>
+            {/* UNCONTROLLED */}
+            <input type="text" ref={(r) => (this.emailRef = r)} />
+            {this.emailRef && this.emailRef.value === '' ? (
+              <small>Email is required.</small>
+            ) : null}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 export default Checkout;
